83484d1bafb3b59d49f212a32a52566ed8bd6299
Cristian Zambrano <criszamcham@gmail.com>
2023-08-02 10:17:07 -0400

    install bootstrap and base controllers

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  root 'pages#home'
+  get 'about', to: 'pages#about'
+  get 'contact', to: 'pages#contact'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
